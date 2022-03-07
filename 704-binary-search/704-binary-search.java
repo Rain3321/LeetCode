d16d42374e7fac9e7cf03561071b1b7da479cede
minwoo.seo <minwoo.seo@kurlycorp.com>
2022-03-07 21:58:43 +0900

    Time: 0 ms (100.00%), Space: 54.1 MB (36.10%) - LeetHub

--- 704-binary-search/704-binary-search.java
+++ 704-binary-search/704-binary-search.java
@@ -1,13 +1,10 @@
 class Solution {
   public int search(int[] nums, int target) {
     int left = 0;
-    int right = nums.length;
+    int right = nums.length - 1;
 
     while (left <= right) {
       int mid = left + (right - left) / 2;
-      if (mid >= nums.length) {
-        break;
-      }
       if (nums[mid] == target) {
         return mid;
       }
