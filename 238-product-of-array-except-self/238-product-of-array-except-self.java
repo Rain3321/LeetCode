083c8caacbc77f846c1a4879106db2b18e2b2ac5
minwoo.seo <minwoo.seo@kurlycorp.com>
2022-03-07 23:08:32 +0900

    Time: 3 ms (51.43%), Space: 58.1 MB (20.37%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -1,31 +1,18 @@
 class Solution {
-public int[] productExceptSelf(int[] sums) {
-    int sum = 1;
-    int excludeSum = 1;
-    int cnt = 0;
-    for (int i : sums) {
-      sum *= i;
-      if (i != 0) {
-        excludeSum *= i;
-      } else {
-        cnt++;
-      }
+  public int[] productExceptSelf(int[] sums) {
+    int[] output = new int[sums.length];
+    int left = 1;
+    int right = 1;
+    for (int i = 0; i < sums.length; i++) {
+      output[i] = left;
+      left *= sums[i];
     }
-    int finalSum = sum;
-    int finalExcludeSum = excludeSum;
-    int finalCnt = cnt;
-    return Arrays.stream(sums)
-      .map(num -> {
-        if (num == 0) {
-          if (finalCnt > 1) {
-            return 0;
-          } else {
-            return finalExcludeSum;
-          }
-        } else {
-          return finalSum / num;
-        }
-      })
-      .toArray();
+
+    for (int i = sums.length - 1; i >= 0; i--) {
+      output[i] *= right;
+      right *= sums[i];
+    }
+    
+    return output;
   }
 }
